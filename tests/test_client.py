99cf70c738b497f5da2b96af4d17451e3e78143b
Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>
2023-04-19 20:40:14 +0300

    TestClient.authorize decorated with @classmethod, added database test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,16 @@
-from main import app
 from fastapi.testclient import TestClient as FastApiTestClient
 from unittest import TestCase
+from classes.database import CollectionProvider
+from main import app
 
 
 class TestClient(TestCase):
     client = FastApiTestClient(app)
 
-    def authorize(self, username: str, password: str):
-        access_token = self.client.post("/token", data={"username": username, "password": password}).json()['access_token']
+    @classmethod
+    def authorize(cls, username: str, password: str):
+        access_token = cls.client.post("/token", data={"username": username, "password": password}).json()['access_token']
         return {"Authorization": f"Bearer {access_token}"}
+
+    def test_db(self):
+        self.assertEqual(CollectionProvider.get_database_name(), "test_curierat")
